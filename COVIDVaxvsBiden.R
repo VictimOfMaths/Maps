6470a973c20e2050ff196bf9cbbd199a58a87b51
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-07-07 10:31:14 +0100

    Update COVIDVaxvsBiden.R

--- COVIDVaxvsBiden.R
+++ COVIDVaxvsBiden.R
@@ -8,7 +8,8 @@ library(gtools)
 library(sf)
 library(tigris)
 library(extrafont)
-library(ragg)+
+library(ragg)
+library(ggtext)
 library(cowplot)
 
 theme_custom <- function() {
@@ -47,6 +48,9 @@ votedata <- read.csv(tempvote) %>%
   set_names(c("FIPS", "per_dem"))
 
 data <- merge(vaxdata, votedata)
+
+#Save data for anyone who wants to draw their own map
+write.csv("Data/COVIDVaxvsVBiden.csv")
 
 #Scatter plot
 agg_tiff("Outputs/COVIDVaxvsBiden.tiff", units="in", width=7, height=7, res=800)
@@ -103,28 +107,49 @@ key <- ggplot(keydata)+
   theme(
     axis.title = element_text(size = 9),axis.line=element_blank(), 
     axis.ticks=element_blank(), axis.text=element_blank(),
-    text=element_text(family="Lato"))+
+    text=element_text(family="Lato", colour="White"),
+    plot.background=element_rect(fill="Grey10", colour="Grey10"))+
   # quadratic tiles
   coord_fixed()
 
+#Pull in county-level map
 shapefile <- counties(resolution="500k")
+
+stateshapes <- states(resolution="500k")
 
 mapdata <- left_join(shapefile, plotdata)
 
 map <- ggplot(mapdata, aes(geometry=geometry, fill=fillcolour))+
   geom_sf(colour="white", size=0.1)+
-  xlim(-130, -65)+
+  geom_sf(data=stateshapes, aes(geometry=geometry), fill=NA, )
+  xlim(-125, -66)+
   ylim(26, 46)+
   scale_fill_identity()+
   theme_void()+
-  labs(title="  US Counties with high vaccination rates are overwhelmingly Democrat-leaning",
-       subtitle="  Proportion of votes case for Joe Biden in the 2020 US presidential election against 1st dose\n  COVID vaccination coverage in US counties reporting data for both.\n",
-       caption="Vaccination data from CDC | Voting data from Tony McGovern | Plot by @VictimOfMaths   ")+
-  theme(text=element_text(family="Lato"), plot.title.position="plot",
-        plot.caption.position="plot", plot.title=element_text(face="bold", size=rel(1.6)))
-       
-agg_tiff("Outputs/COVIDVaxvsBidenMap.tiff", units="in", width=10, height=6, res=800)
+  labs(title="US Counties with high vaccination rates are overwhelmingly Democrat-leaning",
+       subtitle="Proportion of votes case for Joe Biden in the 2020 US presidential election compared to the proportion of the population who have received\nat least 1 dose of COVID vaccine in US counties. Counties with missing vaccination data appear in black.\n",
+       caption="Vaccination data from CDC | Voting data from Tony McGovern | Plot by @VictimOfMaths")+
+  theme(text=element_text(family="Lato", colour="White"), plot.title.position="plot",
+        plot.caption.position="plot", plot.title=element_text(face="bold", size=rel(1.6)),
+        plot.background=element_rect(fill="Grey10", colour="Grey10"),
+        plot.margin=unit(c(0.2,0.2,0.2,0.2), "cm"))+
+#These boxes need moving about, but even just adding one of them in increases the rendering time of the plot *massively* for reasons I don't understand
+  geom_textbox(aes(x=-120, y=35, label="<span style='color:#ff3968;'>Red<span style='color:white;'> areas are GOP-leaning and have high vaccine coverage"), 
+              width=grid::unit(0.1, "npc"), hjust=0, vjust=1, halign=0.5, size=rel(2.5),
+               fill="Grey10", colour="Grey10", box.padding = grid::unit(rep(0, 4), "pt"))+
+  geom_textbox(aes(x=-100, y=29, label="<span style='color:#6d87cc;'>Blue<span style='color:white;'> areas are Dem-leaning and have high vaccine coverage"), 
+               width=grid::unit(0.1, "npc"), hjust=0, vjust=1, halign=0.5, size=rel(2.5),
+               fill="Grey10", colour="Grey10", box.padding = grid::unit(rep(0, 4), "pt"))+
+  geom_textbox(aes(x=-85, y=45, label="<span style='color:#f0f0f0;'>White<span style='color:white;'> areas are GOP-leaning and have low vaccine coverage"), 
+               width=grid::unit(0.1, "npc"), hjust=0, vjust=1, halign=0.5, size=rel(2.5),
+               fill="Grey10", colour="Grey10", box.padding = grid::unit(rep(0, 4), "pt"))+
+  geom_textbox(aes(x=-80, y=32, label="<span style='color:#00cfc1;'>Turquoise<span style='color:white;'> areas are Dem-leaning and have low vaccine coverage"), 
+               width=grid::unit(0.1, "npc"), hjust=0, vjust=1, halign=0.5, size=rel(2.5),
+               fill="Grey10", colour="Grey10", box.padding = grid::unit(rep(0, 4), "pt"))
+
+  
+agg_tiff("Outputs/COVIDVaxvsBidenMap.tiff", units="in", width=10, height=5.4, res=200)
 ggdraw()+
   draw_plot(map, 0,0,1,1)+
-  draw_plot(key, 0.01,0.01,0.3,0.3)
+  draw_plot(key, 0,0.01,0.3,0.3)
 dev.off()
